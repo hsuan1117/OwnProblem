40d8fdb1bfc376390496850c96e63fde1cc228b6
Hsuan (Mom's old Acer) <hsuan.dev@gmail.com>
2020-06-29 00:57:54 +0800

    sync

--- 資訊期末考/PerfectCoin/tests/gen.py
+++ 資訊期末考/PerfectCoin/tests/gen.py
@@ -1,68 +1,43 @@
 import random
 import math
 
-N = random.randrange(2,20)
-Q = random.randrange(2,20)
-all = 0
-factors = []
+taskNum  = 5
+priceMax = 120
+NMax     = 100
+QMax     = 30
 
-print(N)
-for i in range(N):
-    a,b = (random.randrange(1,20),random.randrange(1,20))
-    all = all + a*b
-    print(a,b)
-
-#print(all)
-for i in [y*y for y in range(int(math.sqrt(all))) if y > 0]:
-    if all % i == 0 :
-        factors.append((i,int(all/i)))
-
-print(Q)
-for i in range(Q):
-    fs = random.choice(factors)
-    print(fs[0],fs[1])
-
-
-""" 
 with open(f"Input/task{taskNum:02}.txt","w") as f:
-    
-    f.write(f"{random.randrange(a,b)} {random.randrange(a,b)}")
-    f.write("\n")
+    for _ in range(1):
+        N = random.randrange(2,NMax)
+        Q = random.randrange(2,QMax)
+        all = 0
+        factors = []
+        coins   = [False for x in range(priceMax)]
+        
+        f.write(f"{N}\n")
+        for i in range(N):
+            a,b = (random.randrange(1,2),random.randrange(1,priceMax))
+            while coins[b]==True:
+                b = random.randrange(1,N)
+                if not coins[b] : break
+            coins[b] = True
+            all = all + a*b
+            f.write(f"{a} {b}\n")
+        
+        for i in [y*y for y in range(int(math.sqrt(all))) if y > 0]:
+            xx = int(math.sqrt(i))
+            if all % xx == 0 : factors.append((xx,int(all/xx-xx)))
 
-"""
-
-"""
-int sum;
-vector<pair<int,int>>factor;
-int a,b,n,q,k;
-
-    srand(time(NULL));
-    //get n
-    n = rand()%19;
-    cout << n+1 << endl;
-    //get a,b
-    for(int i=0;i<n+1;i++){
-        a = rand()%20;
-        b = rand()%20;
-        sum += a*b;
-        cout << a << ' ' << b << endl;
-    }
-    
-    //get factor of sum
-    for(int i=1;i*i<=sum;i++){
-        if(sum%i==0)factor.push_back({i,sum/i-i});
-    }
-    //cout << endl << sum << endl << endl;
-    //get q
-    q = rand()%100;
-    cout << q << endl;
-    //get x,y
-    int siz = factor.size();
-    for(int i=0;i<q;i++){
-        k = rand()%siz;
-        if(sum%(factor[k].first+factor[k].second)!=0)cout << "no ";
-        cout << factor[k].first << ' ' << factor[k].second << endl;
-    }
-}
-
-"""
+        f.write(f"{Q}\n")
+        for i in range(Q):
+            decide = random.randrange(1,999)
+            fs = random.choice(factors)
+            if decide % 2 == 1:
+                f.write(f"{fs[0]} {fs[1]}\n")
+            else:
+                tmp  = random.randrange(1,10)
+                XorY = random.randrange(1,999)
+                if XorY % 2 == 1:
+                    f.write(f"{fs[0]} {fs[1]+tmp}\n")
+                else:
+                    f.write(f"{fs[0]+tmp} {fs[1]}\n")
